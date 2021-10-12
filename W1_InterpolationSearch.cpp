33aa6af82004a00bd48d823ba795e3f6e7e61474
vanloc1808 <vanloc1808@users.noreply.github.com>
2021-10-12 16:56:15 +0700

    week 1

---
+++ W1_InterpolationSearch.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int interpolationSearch(vector<int> a, int n, int x) {
+    int l = 0, r = n - 1;
+
+    while(l <= r) {
+        int pivot = (1.0 * (x - a[l]) * (r - l)) / (a[r] - a[l]) + l;
+
+        if (a[pivot] == x) {
+            return pivot;
+        }
+
+        if (a[pivot] > x) {
+            r = pivot - 1;
+        } else {
+            l = pivot + 1;
+        }
+    }
+    return -1;
+}
+
+int main(int argc, char** argv) {
+    int x = atoi(argv[1]);
+
+    vector<int> a;
+    for (int i = 2; i < argc; i++) {
+        a.push_back(atoi(argv[i]));
+    }
+
+    cout << interpolationSearch(a, a.size(), x);
+
+    return 0;
+}
