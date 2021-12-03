166bb0b7ccd2c02599254c3898e002597fe020ca
Nghia Vo <nhannghia2@live.com>
2021-12-03 11:34:19 +0700

    Completed W4.minHeap

--- W4_PriorityQueue.cpp
+++ W4_PriorityQueue.cpp
@@ -1,5 +1,7 @@
 #include <algorithm>
+#include <exception>
 #include <iostream>
+#include <iterator>
 #include <string>
 #include <vector>
 using std::string;
@@ -9,9 +11,17 @@ struct Object {
   unsigned order;
   unsigned priority;
   bool operator>(Object const& other) { return priority > other.priority; }
+  bool operator==(Object const& other) { return priority == other.priority; }
+  bool operator<(Object const& other) { return priority < other.priority; }
 };
 
 using vobj = std::vector<Object>;
+
+// Swap but keep order
+void swap(Object& a, Object& b) {
+  std::swap(a.id, b.id);
+  std::swap(a.priority, b.priority);
+}
 
 class PQMinHeap {
  private:
@@ -31,7 +41,7 @@ class PQMinHeap {
       }
 
       if (swap == root) return;
-      std::swap(m_arr[swap], m_arr[root]);
+      ::swap(m_arr[swap], m_arr[root]);
       root = swap;
     }
   }
@@ -42,7 +52,7 @@ class PQMinHeap {
     while (child > 0) {
       int parent = (child - 1) / 2;
       if (m_arr[parent] > m_arr[child]) {
-        std::swap(m_arr[parent], m_arr[child]);
+        ::swap(m_arr[parent], m_arr[child]);
         child = parent;
       } else {
         return;
@@ -60,6 +70,21 @@ class PQMinHeap {
     }
   }
 
+  // int binary_search(unsigned priority) {
+  //   int l = 0;
+  //   int r = m_arr.size();
+  //   while (l <= r) {
+  //     int m = l + (r - l) / 2;
+  //     if (m_arr[m].priority == priority) return m;
+  //     if (m_arr[m].priority > priority) {
+  //       r = m - 1;
+  //     } else {
+  //       l = m + 1;
+  //     }
+  //   }
+  //   return -1;
+  // }
+
  public:
   PQMinHeap() {}
   bool isEmpty() { return m_arr.empty(); }
@@ -69,9 +94,55 @@ class PQMinHeap {
     m_arr.back().order = n - 1;
     siftUp(n - 1);
   }
-  Object Extract(unsigned prio) {}
-  void Remove(string const& id) {}
-  void changePriority(string const& id, unsigned newOrd) {}
+  Object Extract() {
+    if (isEmpty()) {
+      throw std::runtime_error("Can't extract empty heap!");
+      return Object{};
+    }
+
+    Object data = m_arr[0];
+    ::swap(m_arr[0], m_arr.back());
+    m_arr.pop_back();
+    siftDown(0, m_arr.size() - 1);
+    return data;
+  }
+  void Remove(string const& id) {
+    auto it = std::find_if(m_arr.begin(), m_arr.end(),
+                           [&id](Object const& elem) { return elem.id == id; });
+    if (it == m_arr.end()) return;  // Not found
+    if (it == m_arr.begin()) {      // Delete first element
+      Extract();
+      return;
+    }
+    ::swap(*it, m_arr.back());
+    m_arr.pop_back();
+
+    int current = std::distance(m_arr.begin(), it);
+    int parent = (current - 1) / 2;
+    if (m_arr[current] > m_arr[parent]) {
+      siftUp(current);
+    } else {
+      siftDown(current, m_arr.size() - 1);
+    }
+  }
+  void changePriority(string const& id, unsigned newPrio) {
+    auto it = std::find_if(m_arr.begin(), m_arr.end(),
+                           [&id](Object const& elem) { return elem.id == id; });
+    if (it == m_arr.end()) return;  // Not found
+    if (it == m_arr.begin()) {
+      siftDown(0, m_arr.size() - 1);
+      return;
+    }
+
+    int current = std::distance(m_arr.begin(), it);
+    int parent = (current - 1) / 2;
+    it->priority = newPrio;
+    if (m_arr[current] < m_arr[parent]) {
+      siftUp(current);
+    } else {
+      siftDown(current, m_arr.size() - 1);
+    }
+  }
   void print() {
     for (auto i : m_arr) {
       std::cout << i.id << " " << i.priority << " " << i.order << "\n";
@@ -81,10 +152,37 @@ class PQMinHeap {
 
 int main() {
   PQMinHeap pq;
+  std::cout << "--------------------\n";
   pq.Insert(Object{"abc", 1, 2});
+  pq.print();
+  std::cout << "\n";
   pq.Insert(Object{"bbc", 1, 3});
+  pq.print();
+  std::cout << "\n";
   pq.Insert(Object{"cbc", 1, 5});
+  pq.print();
+  std::cout << "\n";
   pq.Insert(Object{"dbc", 1, 1});
   pq.print();
+  std::cout << "\n";
+  pq.Insert(Object{"ebc", 1, 9});
+  pq.print();
+  std::cout << "\n";
+  pq.Insert(Object{"fbc", 1, 0});
+  pq.print();
+  std::cout << "\n";
+  std::cout << "--------------------\n";
+
+  std::cout << "Extract: " << pq.Extract().id << "\n";
+  pq.print(); std::cout << "\n";
+
+  std::cout << "Remove cbc: \n";
+  pq.Remove("cbc");
+  pq.print(); std::cout << "\n";
+
+  std::cout << "Change priority ebc to 0\n";
+  pq.changePriority("ebc", 0);
+  pq.print(); std::cout << "\n";
+
   return 0;
 }
